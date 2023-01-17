840fc1a56f870f27a2135c3fca813d033b635811
VivianaS14 <vasorianp@gmail.com>
2023-01-17 14:30:16 -0500

    feat: react enviroment

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter();
+
+const App = () => {
+  return <div>Soy App</div>;
+};
+
+export default App;
