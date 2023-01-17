1b65427ab6562dc6a951e96f22f3f5f54cb6c987
VivianaS14 <vasorianp@gmail.com>
2023-01-17 15:27:49 -0500

    feat: added api

---
+++ src/api/users.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://localhost:4000"
+})
+
+export const getUsers = async () => {
+    let res = await api.get("/users")
+    return res
+}
+
+export const createUser = async (_newUser) => {
+    let res = await api.post("/users", _newUser)
+    return res
+}
--- src/container/CreateUser.jsx
+++ src/container/CreateUser.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import UserForm from "../components/UserForm";
+import { createUser } from "../api/users";
 
 const CreateUser = () => {
+  const [error, setError] = useState("");
+  const [IsLoading, setIsLoading] = useState(false);
+
+  const sendUser = async (newUser) => {
+    try {
+      setIsLoading(true);
+      await createUser(newUser);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <>
       <h2>Create User.jsx</h2>
