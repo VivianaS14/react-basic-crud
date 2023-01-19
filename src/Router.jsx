3500c02221faf2bcb104f0479c9fccc795c286f6
VivianaS14 <vasorianp@gmail.com>
2023-01-19 15:13:10 -0500

    feat: Router

--- src/Router.jsx
+++ src/Router.jsx
@@ -9,18 +9,20 @@ import {
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Admin from "./Admin";
+import Users from "./Users";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Navbar />}>
       <Route path="/" element={<Home />} />
       <Route path="/admin" element={<Admin />} />
+      <Route path="/users" element={<Users />} />
     </Route>
   )
 );
 
-const App = () => {
+const AppRouter = () => {
   return <RouterProvider router={router} />;
 };
 
-export default App;
+export default AppRouter;
